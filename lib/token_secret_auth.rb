540ad79835efd7337243f58ff3804fdc3bab2d19
tgaff <tgaff@alumni.nd.edu>
2018-01-15 14:30:55 -0800

    re-architect for single internal Hashids instance stored on eigenclass

--- lib/token_secret_auth.rb
+++ lib/token_secret_auth.rb
@@ -1,5 +1,94 @@
 require "token_secret_auth/version"
-
+require 'pry'
 module TokenSecretAuth
-  # Your code goes here...
+  def decode_token(token)
+    decoded = TokenSecretAuth.hash_id.decode(token).first
+  end
+
+  def token
+    return nil if !id
+    encode(id)
+  end
+
+  # the model can call this method to generate a new password for the user
+  # it should then encrypt this password for storage in db
+  def generate_secret
+    self.class.generate_secret
+  end
+
+  def self.included(base)
+    base.extend(ClassMethods)
+
+  end
+
+  # eigenclass
+  class << self
+    def hash_id
+      @hid
+    end
+    def set_hash_id_instance(salt)
+      @hid = Hashids.new(salt, 12)
+    end
+  end
+
+  module ClassMethods
+    require 'hashids'
+    attr_reader :hash_ids
+    def find_with_token(token)
+      begin
+        find(TokenSecretAuth.hash_id.decode(token).first)
+      rescue Hashids::InputError
+        # controller should handle not found when we can't decode bad token
+        return find(nil)
+      end
+    end
+
+    def authenticate_by_credentials(token, secret=nil)
+      account = find_with_token(token)
+      # note BCrypt's authenticate will return false or the object when matched
+      if account
+        account.authenticate(secret)
+      end
+    end
+
+    def generate_secret
+      rand(36**secret_length).to_s(36)
+    end
+
+    def secret_length
+      @secret_length ||= 32
+    end
+
+    def hash_id(val)
+      TokenSecretAuth.hash_id(val)
+    end
+
+    # recommended configuration:
+    # TokenSecretAuth.configure do |config|
+    #   config.id_salt = 'some appropriate saltiness'
+    # end
+    def configure
+      yield self
+      config_hashids
+    end
+
+    private
+  end
+  # salt for the id ONLY,
+  # optional config - even if unset the only leak is the id
+  @id_salt = 'some appropriate saltiness'
+
+  # mandatory to have a hash_id instance
+  TokenSecretAuth.set_hash_id_instance(@id_salt)
+
+
+  private
+  def secret_length
+    @secret_length ||= 32
+  end
+
+  def encode(value)
+    TokenSecretAuth.hash_id.encode(value)
+  end
+
 end
--- spec/token_secret_auth_spec.rb
+++ spec/token_secret_auth_spec.rb
@@ -1,11 +1,101 @@
 require "spec_helper"
 
 RSpec.describe TokenSecretAuth do
-  it "has a version number" do
-    expect(TokenSecretAuth::VERSION).not_to be nil
+
+  class Includer
+    include TokenSecretAuth
+    def self.find(v); end
+  end
+  let(:includer) do
+    Includer.new
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#decode_token' do
+    let(:encoded) { Includer.new.send(:encode, 4002) }
+
+    it 'properly decodes the token' do
+      expect(includer.decode_token(encoded)).to eq 4002
+    end
+  end
+
+  describe '#token' do
+    let(:klass) do
+      Struct.new(:id) do
+        include TokenSecretAuth
+      end
+    end
+    let(:test_object) { klass.new(123321) }
+
+    it 'returns the id encoded' do
+      expect(test_object.token).to eq 'bRevJzqDJvyY'
+    end
+  end
+
+  describe 'generate_secret' do
+    it 'generates a new secret' do
+      expect(includer.generate_secret).to_not eq includer.generate_secret
+    end
+
+    it 'generates a secret with length usually ~32' do
+      keys = 10.times.map { includer.generate_secret.length }
+      mean = keys.inject(0, :+) / 10
+      # if this fails it's either a bug or your lucky day
+      expect(mean).to be_between(30,32)
+    end
+  end
+
+  describe 'ClassMethods' do
+    describe '.generate_secret' do
+      it 'generates a new secret' do
+        expect(Includer.generate_secret).to_not eq Includer.generate_secret
+      end
+
+      it 'generates a secret with length usually ~32' do
+        keys = 10.times.map { Includer.generate_secret.length }
+        mean = keys.inject(0, :+) / 10
+        # if this fails it's either a bug or your lucky day
+        expect(mean).to be_between(30,32)
+      end
+    end
+
+    describe '.find_with_token' do
+      it 'finds records by token' do
+        expect(Includer).to receive(:find).with(42)
+        token = TokenSecretAuth.hash_id.encode 42
+        Includer.find_with_token(token)
+      end
+
+      it 'searches for nil if the token is invalid' do
+        expect(Includer).to receive(:find).with(nil)
+        Includer.find_with_token('lvrKq----a')
+      end
+    end
+
+    describe '.authenticate_by_credentials' do
+      class FriendlyFox
+        include TokenSecretAuth
+        def authenticate(secret) # mock for BCrypt authenticate
+          secret == 'secret' ? 'ok' : false
+        end
+        def self.find(x); end
+      end
+
+      let (:double_instance) do
+        double = FriendlyFox.new
+      end
+
+      before do
+        allow(FriendlyFox).to receive(:find_with_token).with('lvrKqvNqR58a')
+            .and_return(double_instance)
+      end
+
+      it 'finds the right record' do
+        expect(FriendlyFox.authenticate_by_credentials('lvrKqvNqR58a', 'secret')).to eq 'ok'
+      end
+
+      it "returns false if the secret doesn't match" do
+        expect(FriendlyFox.authenticate_by_credentials('lvrKqvNqR58a', 'asldkfjklj')).to eq false
+      end
+    end
   end
 end
